6eb3f6af0b961be497e5f143bdc2bc4e153680c4
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-02-28 21:25:12 +0300

    updates on family

--- admin/src/redux/familiesRedux.js
+++ admin/src/redux/familiesRedux.js
@@ -53,10 +53,6 @@ export const familySlice = createSlice({
       state.error = true;
     },
     //UPDATE
-    addfamilyStart: (state) => {
-      state.isFetching = true;
-      state.error = false;
-    },
     addfamilySuccess: (state, action) => {
       state.isFetching = false;
       state.families.push(action.payload);
