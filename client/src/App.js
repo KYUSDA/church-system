b77173367cd81a6eb417b156dac802bffaa1df10
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-09-19 21:40:10 +0300

    updates made on slides

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import { 
-  About, 
-  Footer, 
-  Header, 
+import React from "react";
+import {
+  About,
+  Footer,
+  Header,
   Testimonial,
   Departments,
   Families,
-Announcements,
-Kyusda,
-Leader
-} 
-from './container/index';
-import { Navbar } from './components';
-import './App.scss';
+  Announcements,
+  Kyusda,
+  Leader,
+} from "./container/index";
+import { Navbar } from "./components";
+import Slideshow from "./components/SlideShow/Slideshow";
+import "./App.scss";
 const App = () => (
   <div className="app">
+    <Slideshow />
     <Navbar />
-    <Header />
-    <About />
-    {/* <Leader /> */}
+    {/* <Header /> */}
+    {/* <About /> */}
+    <Leader />
     <Departments />
-<Families />
-<Announcements />
-<Testimonial />
-<Kyusda />
-<Footer />
+    <Families />
+    <Announcements />
+    <Testimonial />
+    {/* <Kyusda /> */}
+    {/* <Footer /> */}
+    {/* New footer */}
   </div>
 );
 
