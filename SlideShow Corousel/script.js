0437fb03045efbe24d2367076e546785d71547f5
Otieno <ronvi035@gmail.com>
2023-10-12 17:52:56 +0300

    working on mobile version

--- SlideShow Corousel/script.js
+++ SlideShow Corousel/script.js
@@ -47,6 +47,7 @@ pauseButton.innerHTML = '&#10074;&#10074;'; // Initially set to the Pause icon
 const mainMenu = document.querySelector(".mainMenu");
 const closeMenu = document.querySelector(".closeMenu");
 const openMenu = document.querySelector(".openMenu");
+const scrollBtn = document.querySelector(".scroll-top-btn");
 
 
 openMenu.addEventListener('click', show);
@@ -56,11 +57,13 @@ closeMenu.addEventListener('click', close);
 function show(){
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
+    scrollBtn.style.display = 'none';
 }
 
 
 function close(){
     mainMenu.style.top = '-100%';
+    scrollBtn.style.display = 'block';
 }
 
 
