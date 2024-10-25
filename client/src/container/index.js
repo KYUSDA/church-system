368374dce15b44dad182b47c1f90d4d71e25fc1e
samuelkamotho92 <samuelkamotho92@gmail.com>
2024-10-25 18:38:52 +0300

    Resolving file name

--- client/src/container/index.js
+++ client/src/container/index.js
@@ -5,5 +5,5 @@ export { default as Families } from "../components/Family/Families";
 export { default as Gallery } from "../components/ImageGallery/ImageGallery";
 export { default as Announcements } from "../components/Announcements/Announcement.jsx";
 export { default as Footer } from "../components/Footer/Footer.jsx";
-export { default as KyuSda } from "../components/Gallery/KyuSdaGallery.js";
+export { default as KyuSda } from "../components/Gallery/KyuSdaGallery.jsx";
 export { default as Leader } from "../components/Leaders";
