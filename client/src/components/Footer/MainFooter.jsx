90f6813b31f1b479c58cdbc3acc66192cce5a316
sammy6378 <samuel202mwangi@gmail.com>
2024-11-04 10:08:49 +0300

    code: update footer section, add privacy policy and terms and conditions

--- client/src/components/Footer/MainFooter.jsx
+++ client/src/components/Footer/MainFooter.jsx
@@ -1,21 +1,64 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+import { FaTwitter,FaFacebook,FaYoutube } from 'react-icons/fa'
 
-const MainFooter = () => {
-    return (
-        <div
-            className="copyright"
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                margin: "auto",
-                width: '100vw'
-            }}
-        >
-            <p className="p-text">@2023 - 2024 KYUSDA CHURCH</p>
-            <p className="p-text">All rights reserved</p>
+function MainFooter() {
+  return (
+    <footer className="footer">
+      <div className="footer_section__container footer__container">
+        <div className="footer__col">
+          <h3>Kirinyaga University<span> Church</span></h3>
+          <p>
+            We are blessed to be a part of your spiritual journey and are dedicated 
+            to fostering faith, hope, and community at every step.
+          </p>
+          <p>
+            Join us in worship, fellowship, and service as we grow together in faith and 
+            love, following the teachings of Christ.
+          </p>
         </div>
-    )
+        
+        <div className="footer__col">
+          <h4>About Us</h4>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#about">About Us</a></p>
+          <p><a href="#ministries">Our Ministries</a></p>
+          <p><a href="#events">Upcoming Events</a></p>
+          <p><Link to={"/kirinyaga-adventist-terms-and-conditions"} target='_blank'>Terms & Conditions</Link></p>
+        </div>
+        
+        <div className="footer__col">
+          <h4>Resources</h4>
+          <p><a href="#sermons">Sermons</a></p>
+          <p><a href="#bible-study">Bible Study</a></p>
+          <p><Link to={"/kirinyaga-adventist-privacy-policy"} target='_blank'>Privacy Policy</Link></p>
+          <p><a href="#volunteer">Volunteer Opportunities</a></p>
+          <p><a href="#donations">Make a Donation</a></p>
+        </div>
+        
+        <div className="footer__col">
+          <h4>Contact Us</h4>
+          <p>
+            <i className="ri-map-pin-2-fill"></i> Kirinyaga University, Kutus, Kirinyaga
+          </p>
+          <p><i className="ri-mail-fill"></i> kyusdachurch@gmail.com</p>
+          <p><i className="ri-phone-fill"></i>  (+254) 797 138885</p>
+        </div>
+      </div>
+      
+      <div className="footer__bar">
+        <div className="footer__bar__content">
+          <p>Copyright © 2024 Kirinyaga University Church. All rights reserved.</p>
+          <div className="footer__socials">
+            <a href='https://www.facebook.com/KyUSDAchurch' target='_blank' rel="noreferrer"><FaFacebook /></a>
+            <a href="https://x.com/kyusdachurch?s=09" target='_blank' rel="noreferrer"><FaTwitter /></a>
+            <a href="http://www.youtube.com/@kyusdachurch" target='_blank' rel="noreferrer"><FaYoutube /></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default MainFooter
