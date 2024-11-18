b52766d82820f1a05a9a9c473cea94d02b7b060a
RONNEY OTIENO <RONNEY.OTIENO@studentambassadors.com>
2024-11-18 06:16:09 +0300

    refactor(Banner): remove commented-out code for cleaner component structure

--- client/src/components/Header/Banner.jsx
+++ client/src/components/Header/Banner.jsx
@@ -1,28 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom';
-// import './Banner.css';
-// const Banner = () => {
-//     return (
-//         <div className='hero-section'>
-//             <div className="hero-text">
-//                 <h1>WELCOME TO KYUSDA CHURCH</h1>
-//                 <p>
-//                     Christ was a Seventh-Day Adventist,
-//                     <br /> to all intents and purposes. <br />
-//                     (Medical Ministry 49.4)
-//                 </p>
-//                 <Link to="/about-section">
-//                     About Us <i className="fa fa-angle-down"></i>
-//                 </Link>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Banner
-
-
-
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Banner.css';
