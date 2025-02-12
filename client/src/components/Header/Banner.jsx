6e48c81b801071e8d8174fd318788faa83ea7fac
sammy6378 <samuel202mwangi@gmail.com>
2025-02-12 17:56:24 +0300

    feat: restructure Banner component to improve layout and text styling; reposition EventsCard

--- client/src/components/Header/Banner.jsx
+++ client/src/components/Header/Banner.jsx
@@ -10,15 +10,19 @@ const Banner = () => {
       {/* Overlay for better text contrast */}
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-      <EventsCard />
-
       {/* Hero Text */}
       <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-        <h1 className="text-4xl md:text-5xl font-semibold">WELCOME TO KYUSDA CHURCH</h1>
-        <p className="mt-4 text-lg md:text-xl">
-          Christ was a Seventh-Day Adventist, <br /> to all intents and purposes. <br />
-          (Medical Ministry 49.4)
+        <h1 className="text-4xl md:text-4xl font-semibold">WELCOME TO KYUSDA CHURCH</h1>
+        <p className="mt-4 text-sm md:text-base">
+            Christ was a Seventh-Day Adventist, <br /> to all intents and purposes. <br />
+            (Medical Ministry 49.4)
         </p>
+
+      </div>
+
+      {/* Event Cards Section */}
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-5xl pb-10">
+        <EventsCard />
       </div>
     </div>
   );
