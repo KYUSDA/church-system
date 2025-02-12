a60437ff5ec1f2bdb6abdd76561969e2b7f96660
sammy6378 <samuel202mwangi@gmail.com>
2025-02-12 16:51:28 +0300

    feat: update Banner component with new background image and remove unused styles

--- client/src/components/Header/Banner.jsx
+++ client/src/components/Header/Banner.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import './Banner.css';
-import EventsCard from '../EventsCard/events_card';
+import React from "react";
+import EventsCard from "../EventsCard/events_card";
 
 const Banner = () => {
-    return (
-        <div className='hero-section'>
-        <EventsCard />
-            {/* Hero Text */}
-            <div className="hero-text">
-                <h1>WELCOME TO KYUSDA CHURCH</h1>
-                <p>
-                    Christ was a Seventh-Day Adventist,
-                    <br /> to all intents and purposes. <br />
-                    (Medical Ministry 49.4)
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className="relative min-h-screen bg-cover bg-bottom bg-no-repeat bg-fixed"
+      style={{ backgroundImage: "url('/kyu.jpg')" }} // Inline style for debugging
+    >
+      {/* Overlay for better text contrast */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+      <EventsCard />
+
+      {/* Hero Text */}
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+        <h1 className="text-4xl md:text-5xl font-semibold">WELCOME TO KYUSDA CHURCH</h1>
+        <p className="mt-4 text-lg md:text-xl">
+          Christ was a Seventh-Day Adventist, <br /> to all intents and purposes. <br />
+          (Medical Ministry 49.4)
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
