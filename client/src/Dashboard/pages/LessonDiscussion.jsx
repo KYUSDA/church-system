1c81e3c484788e5126dece55d66c1e4e4f21401b
Upskillkenya <admin@upskillkenya.co.ke>
2025-03-03 21:17:12 +0300

    feat: add LessonDiscussion component with placeholder message for upcoming feature

---
+++ client/src/Dashboard/pages/LessonDiscussion.jsx
@@ -0,0 +1,12 @@
+
+
+
+const LessonDiscussion = () => {
+    return (
+        <div>
+            <h1 className='absolute text-center text-3xl font-bold mt-10 top-[10%]'>Lesson Discussion coming soon</h1>
+        </div>
+    )
+}
+
+export default LessonDiscussion
