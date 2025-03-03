6361c97daa554db32120e927d69aede9b557f855
Upskillkenya <admin@upskillkenya.co.ke>
2025-03-03 21:17:06 +0300

    feat: add Family component with placeholder message for upcoming page

---
+++ client/src/Dashboard/pages/Family.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Family = () => {
+    return (
+        <div>
+            <h1 className='absolute text-center text-3xl font-bold mt-10 top-[10%]'>Family page coming soon</h1>
+        </div>
+    )
+}
+
+export default Family
