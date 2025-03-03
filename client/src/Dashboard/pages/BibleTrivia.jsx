7dcc092de224d0f52ca413159ce3a044b3cff14d
Upskillkenya <admin@upskillkenya.co.ke>
2025-03-03 21:16:51 +0300

    feat: add BibleTrivia component with placeholder message

---
+++ client/src/Dashboard/pages/BibleTrivia.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const BibleTrivia = () => {
+    return (
+        <div>
+            <h1 className='absolute text-center text-3xl font-bold mt-10 top-[10%]'>Bible trivia coming soon</h1>
+        </div>
+    )
+}
+
+export default BibleTrivia
