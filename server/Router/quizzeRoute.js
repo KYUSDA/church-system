bb3a56201298b474f481f744c15f488bf4035f44
sammy6378 <samuel202mwangi@gmail.com>
2025-03-10 22:10:53 +0300

    feat: remove quizze route and associated controller logic

--- server/Router/quizzeRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createQuiz } from "../Controlers/quizzes.js";
-const quizzeRoute = Router();
-
-quizzeRoute.route("/create-quizze").post(createQuiz);
-
-
-export default quizzeRoute;
