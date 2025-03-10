cc4b0ec258563e6c36987a4a8e94bb4fdf2b7c37
sammy6378 <samuel202mwangi@gmail.com>
2025-03-10 22:09:59 +0300

    feat: add authentication routes for sign up, sign in, and password reset

---
+++ server/Router/authRoute.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import memberAuth from "../Controlers/authControler";
+const authRoute = Router();
+
+authRoute.route("/signUp").post(memberAuth.memberSignUp);
+
+authRoute.route("/signIn").post(memberAuth.memberSignIn);
+
+authRoute.route("/resetToken").post(memberAuth.memberResetToken);
+
+authRoute.route("/resetPassword/:token").patch(memberAuth.resetPassword);
+
+export default authRoute;
