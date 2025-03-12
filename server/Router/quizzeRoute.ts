7bbc76bb5dbc8312a3d63fbd561a7cd63ca96657
sammy6378 <samuel202mwangi@gmail.com>
2025-03-12 14:17:24 +0300

    feat: enhance quizzeRoute with additional endpoints for retrieving quizzes and submitting results

--- server/Router/quizzeRoute.ts
+++ server/Router/quizzeRoute.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { createQuiz } from "../Controlers/quizzes";
+import { createQuiz, getCompletedQuizzes, getQuiz, sendQuizResults } from "../Controlers/quizzes";
+import requireAuth from "../middleware/authmiddleware";
 const quizzeRoute = Router();
 
 quizzeRoute.route("/create-quizze").post(createQuiz);
+
+quizzeRoute.get("/get-quizze/:id",getQuiz);
+
+quizzeRoute.route("/get-quizzes").get(getCompletedQuizzes);
+
+quizzeRoute.post("/forward-results", requireAuth, sendQuizResults);
 
 
 export default quizzeRoute;
