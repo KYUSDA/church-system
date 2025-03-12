88b1355ca64464e448f5acd521f2491e1b4ae266
sammy6378 <samuel202mwangi@gmail.com>
2025-03-12 14:16:37 +0300

    feat: refactor requireAuth middleware to use JWT verification and improve error handling

--- server/middleware/authmiddleware.ts
+++ server/middleware/authmiddleware.ts
@@ -1,9 +1,11 @@
-import authModel from "../Models/authModel.js";
-import { verify, JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import { redis } from "../utils/redis.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
+import { catchAsyncErrors } from "./catchAsyncErrors.js";
 
-const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
-  // verify user is authenticated
+const requireAuth = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+  // Verify user is authenticated
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -16,20 +18,21 @@ const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
     if (!process.env.SECRET) {
       throw new Error("SECRET environment variable is not defined");
     }
-    const { _id } = verify(token, process.env.SECRET) as JwtPayload;
 
-    const user = await authModel.findOne({ _id }).select("_id");
+    const decoded = jwt.verify(token, process.env.SECRET) as jwt.JwtPayload; // ✅ Correct way to use it
+    const userId = decoded._id;
+
+    const user = await redis.get(userId);
     if (!user) {
-      return res.status(401).json({ error: "User not found" });
+      return next(new ErrorHandler("User session not found. Please log in", 404));
     }
 
-    req.user = user;
-
+    req.user = JSON.parse(user);
     next();
   } catch (error) {
     console.log(error);
     res.status(401).json({ error: "Request is not authorized" });
   }
-};
+});
 
 export default requireAuth;
