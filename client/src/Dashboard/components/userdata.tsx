cbdbe5a164c1555e55a394df9542ba86bc76603b
sammy6378 <samuel202mwangi@gmail.com>
2025-03-14 13:25:09 +0300

    feat: add custom hook for fetching user data

---
+++ client/src/Dashboard/components/userdata.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { getBaseUrl } from "../../services/authService";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+
+interface TUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  registration: string;
+  course: string;
+  year: string;
+  role: string;
+  phoneNumber: string;
+  avatar: { url: string };
+}
+const useUserData = () => {
+  const authState = useSelector((state:RootState) => state.auth);
+  const user =  authState.user as TUser | null;
+
+  const accessToken = authState.accessToken as string | null;
+  const [userData, setUserData] = useState<TUser | null>(null);
+  const baseUrl = getBaseUrl();
+
+  useEffect(() => {
+    if (!user || !user.id || !accessToken) return;
+
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/user/${user.id}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, [user, baseUrl, accessToken]);
+
+  return { userData, setUserData, user }; // Return user from Redux for easier access
+};
+
+export default useUserData;
