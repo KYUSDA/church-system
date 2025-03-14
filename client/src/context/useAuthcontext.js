b9484d822acea07821adaea1df17882117190d4d
sammy6378 <samuel202mwangi@gmail.com>
2025-03-14 13:23:16 +0300

    chore: remove unused useAuthcontext file as part of code cleanup

--- client/src/context/useAuthcontext.js
+++
@@ -1,6 +0,0 @@
-import { AuthContext } from "./authcontext";
-import { useContext } from "react";
-export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  return context;
-};
