9f857cc9fc7c6160caabcbbff4038687f014ea8d
sammy6378 <samuel202mwangi@gmail.com>
2025-03-14 13:40:06 +0300

    feat: refactor updateUserAvatar to use catchAsyncErrors middleware and improve error handling

--- server/Controlers/userControler.ts
+++ server/Controlers/userControler.ts
@@ -1,7 +1,15 @@
 import Member from "../Models/authModel";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { Multer } from "multer";
+
+interface MulterRequest extends Request {
+  file?: Express.Multer.File;
+}
+
 import cloudinary from "cloudinary";
 import authModel from "../Models/authModel";
+import ErrorHandler from "../utils/ErrorHandler";
+import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
 export async function getAll(req: Request, resp: Response) {
   try {
     const query = req.query.new;
@@ -77,37 +85,75 @@ export async function createUser(req:Request, resp:Response) {
 }
 
 //update user avatar
-export const updateUserAvatar = async (req: Request, res: Response) => {
+// export const updateUserAvatar = async (req: Request, res: Response) => {
+//   try {
+//     const { avatar } = req.body;
+//     const { id } = req.params; // Get user ID from params
+//     console.log(avatar);
+//     const user = await authModel.findById(id);
+//     if (!user) {
+//      res.status(404).json({ success: false, message: "User not found. Please login again" });
+//     }
+
+//     if (avatar && user) {
+//       // Delete the old avatar from Cloudinary (if exists)
+//       if (user.avatar?.public_id) {
+//         await cloudinary.v2.uploader.destroy(user.avatar.public_id.toString());
+//       }
+
+//       // Upload new avatar to Cloudinary
+//       const myCloud = await cloudinary.v2.uploader.upload(avatar, { folder: "Avatars" });
+
+//       // Update user's avatar data
+//       user.avatar = {
+//         public_id: myCloud.public_id,
+//         url: myCloud.secure_url,
+//       };
+
+//       await user.save();
+//     }
+
+//     res.status(200).json({ success: true, user, message: "User profile updated" });
+//   } catch (err) {
+//     console.error(err);
+//    res.status(500).json({ success: false, messsage:"Internal Server Error" });
+// };
+
+
+export const updateUserAvatar =catchAsyncErrors (async (req: MulterRequest, res: Response, next:NextFunction) => {
+
   try {
-    const { avatar } = req.body;
-    const { id } = req.params; // Get user ID from params
-    console.log(avatar);
+    const { id } = req.params;
     const user = await authModel.findById(id);
+
     if (!user) {
-     res.status(404).json({ success: false, message: "User not found. Please login again" });
+      return next(new ErrorHandler("User not found. Please login again",404));
     }
 
-    if (avatar && user) {
-      // Delete the old avatar from Cloudinary (if exists)
-      if (user.avatar?.public_id) {
-        await cloudinary.v2.uploader.destroy(user.avatar.public_id.toString());
-      }
-
-      // Upload new avatar to Cloudinary
-      const myCloud = await cloudinary.v2.uploader.upload(avatar, { folder: "Avatars" });
-
-      // Update user's avatar data
-      user.avatar = {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url,
-      };
-
-      await user.save();
+    if (!req.file) {
+      return res.status(400).json({ success: false, message: "No file uploaded" });
     }
+
+    // Delete old avatar from Cloudinary (if exists)
+    if (user.avatar?.public_id) {
+      await cloudinary.v2.uploader.destroy(user.avatar.public_id.toString());
+    }
+
+    // Upload new avatar to Cloudinary
+    const myCloud = await cloudinary.v2.uploader.upload(req.file.path, {
+      folder: "Avatars",
+    });
+
+    // Update user's avatar
+    user.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+
+    await user.save();
 
     res.status(200).json({ success: true, user, message: "User profile updated" });
-  } catch (err) {
-    console.error(err);
-   res.status(500).json({ success: false, messsage:"Internal Server Error" });
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message, 400));
   }
-};
+});
