631076cc8b56dc628fbf37edce1f8d3bc9415456
sammy6378 <samuel202mwangi@gmail.com>
2025-03-14 13:39:38 +0300

    feat: add cookie parser and improve CORS configuration with allowed origins

--- server/app.ts
+++ server/app.ts
@@ -8,27 +8,41 @@ import familyRoute from "./Router/familiesRouter";
 import quizzeRoute from "./Router/quizzeRoute";
 import devotionRoute from "./Router/devotionRoute";
 import prayerRequestRouter from "./Router/prayerRoute";
-
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// body parser
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
 
 app.use(json());
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://jolly-coast-0386d1803.5.azurestaticapps.net",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://jolly-coast-0386d1803.5.azurestaticapps.net",
-      "https://jolly-coast-0386d1803.5.azurestaticapps.net/",
-      "*",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
+
 
 app.use("/kyusda/v1/member/", memberRoute);
 app.use("/kyusda/v1/department/", departmentRoute);
