f12a31420437e9d386345681347d86df7f405467
sammy6378 <samuel202mwangi@gmail.com>
2025-03-14 13:22:56 +0300

    chore: remove unused index.js file as part of code cleanup

--- client/src/container/index.js
+++
@@ -1,9 +0,0 @@
-export { default as About } from "../components/About/About";
-export { default as Testimonial } from "../components/Testimonials/Testimonials";
-export { default as Departments } from "../components/Department/Departments.jsx";
-export { default as Families } from "../components/Family/Families";
-export { default as Gallery } from "../components/ImageGallery/ImageGallery";
-export { default as Announcements } from "../components/Announcements/Announcement.jsx";
-export { default as Footer } from "../components/Footer/Footer.jsx";
-export { default as KyuSda } from "../components/Gallery/KyuSdaGallery.jsx";
-export { default as Leader } from "../components/Leaders";
