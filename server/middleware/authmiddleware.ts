6359025f94085ccf3ed25c47964e3355ebc7a3ae
sammy6378 <samuel202mwangi@gmail.com>
2025-03-14 13:40:18 +0300

    feat: enhance requireAuth middleware to improve authentication flow and error handling

--- server/middleware/authmiddleware.ts
+++ server/middleware/authmiddleware.ts
@@ -1,38 +1,36 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from "express";
 import { redis } from "../utils/redis.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
 
-const requireAuth = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
-  // Verify user is authenticated
-  const { authorization } = req.headers;
 
-  if (!authorization) {
-    return res.status(401).json({ error: "Authorization token required" });
-  }
+export const requireAuth = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const access_token = req.cookies.access_token;
 
-  const token = authorization.split(" ")[1];
+        if(!access_token) {
+            return next(new ErrorHandler("Session not found. Please log in again...", 401));
+        }
 
-  try {
-    if (!process.env.SECRET) {
-      throw new Error("SECRET environment variable is not defined");
+        const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN as string) as JwtPayload;
+        if(!decoded) {
+            return next(new ErrorHandler("Authentication failed", 401));
+        };
+
+       // const user = await userModel.findById(decoded.id);
+       const user = await redis.get(decoded.id);
+       if(!user) {
+        return next(new ErrorHandler("User session not found. Please log in", 404));
+       }
+
+       req.user = JSON.parse(user);
+       next();
+        
+    } catch (error: any) {
+        console.log("Authentication failed");
     }
+})
 
-    const decoded = jwt.verify(token, process.env.SECRET) as jwt.JwtPayload; // ✅ Correct way to use it
-    const userId = decoded._id;
-
-    const user = await redis.get(userId);
-    if (!user) {
-      return next(new ErrorHandler("User session not found. Please log in", 404));
-    }
-
-    req.user = JSON.parse(user);
-    next();
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ error: "Request is not authorized" });
-  }
-});
 
 export default requireAuth;
