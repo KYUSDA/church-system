1a4f8507f5bdfcc706415a5040781f5f335f7354
sammy6378 <samuel202mwangi@gmail.com>
2025-03-17 21:28:37 +0300

    feat: add endpoints for score management and trivia updates with authentication

--- server/Router/userRouter.ts
+++ server/Router/userRouter.ts
@@ -6,11 +6,18 @@ import {
   updateUser,
   deleteUser,
   updateUserAvatar,
+  updateScore,
+  getAllScores,
+  updateTriviaNumbers,
 } from "../Controlers/userControler";
+import requireAuth from "../middleware/authmiddleware";
 const userRouter = Router();
 userRouter.route("/getUsers").get(getAll);
 userRouter.route("/createUser").post(createUser);
 userRouter.put("/update-avatar/:id",updateUserAvatar);
+userRouter.put("/update-scores",requireAuth, updateScore);
+userRouter.put("/update-trivias",requireAuth,updateTriviaNumbers);
+userRouter.get("/get-scores",getAllScores);
 userRouter.route("/:id").get(getOne).patch(updateUser).delete(deleteUser);
 
 export default userRouter;
