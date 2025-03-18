31f57e723caef29933f10595475af7193bec6631
sammy6378 <samuel202mwangi@gmail.com>
2025-03-18 16:27:33 +0300

    feat: enhance SignIn component layout and improve form structure

--- client/src/Auth/SignIn.tsx
+++ client/src/Auth/SignIn.tsx
@@ -14,7 +14,6 @@ import { Visibility, VisibilityOff, LockOutlined as LockOutlinedIcon } from "@mu
 import { useLogin } from "../hooks/userLoginHook";
 import AuthLayout from "./AuthLayout";
 
-
 const SignInSide = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,16 +21,15 @@ const SignInSide = () => {
   const [showPassword, setShowPassword] = useState(false);
   const { loginUser, error } = useLogin();
 
-
   interface formData {
     email: string;
     password: string;
   }
 
-  const values: formData ={
+  const values: formData = {
     email,
-    password
-  }
+    password,
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -44,66 +42,82 @@ const SignInSide = () => {
 
   return (
     <AuthLayout>
-      <Box className="w-full max-w-md flex flex-col items-center">
-        <Avatar className="m-2" sx={{ bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
+      <Box className="w-full flex items-center justify-center h-screen">
+        <Box className="bg-white rounded-md shadow-md shadow-gray-400 max-w-md w-full p-6 max-md:py-4 max-500:p-2 mx-2">
+          <Box className="flex flex-col items-center">
+            <Avatar className="m-2" sx={{ bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
 
-        {/* Sign-in Form */}
-        <Box component="form" noValidate onSubmit={handleSubmit} className="w-full mt-4">
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Email Address"
-            id="email"
-            autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            error={email === ""}
-            helperText={email === "" ? "Email is required" : ""}
-          />
+            {/* Sign-in Form */}
+            <Box component="form" noValidate onSubmit={handleSubmit} className="w-full mt-4">
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Email Address"
+                id="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={email === ""}
+                helperText={email === "" ? "Email is required" : ""}
+              />
 
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Password"
-            type={showPassword ? "text" : "password"}
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            error={password === ""}
-            helperText={password === "" ? "Password is required" : ""}
-            InputProps={{
-              endAdornment: (
-                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              ),
-            }}
-          />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                error={password === ""}
+                helperText={password === "" ? "Password is required" : ""}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  ),
+                }}
+              />
 
-          <FormControlLabel
-            control={<Checkbox color="primary" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />}
-            label="Remember me"
-          />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="primary"
+                    checked={rememberMe}
+                    onChange={(e) => setRememberMe(e.target.checked)}
+                  />
+                }
+                label="Remember me"
+              />
 
-          {error && <p className="text-red-400 text-md">{error}</p>}
+              {error && <p className="text-red-400 text-md">{error}</p>}
 
-          <Button type="submit" fullWidth variant="contained" className="mt-3 mb-2" disabled={!isFormValid}>
-            Sign In
-          </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                className="mt-3 mb-2"
+                disabled={!isFormValid}
+              >
+                Sign In
+              </Button>
 
-          <div className="flex justify-between text-sm mt-3 w-full">
-            <Link href="/resetToken">Forgot password?</Link>
-            <Link href="/signUp">{"Don't have an account? Sign Up"}</Link>
-          </div>
+              <div className="flex justify-between text-sm mt-3 w-full">
+                <Link href="/resetToken">Forgot password?</Link>
+                <Link href="/signUp">{"Don't have an account? Sign Up"}</Link>
+              </div>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </AuthLayout>
