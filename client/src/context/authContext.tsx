622d1f670404e4de272504f89c56f4b6bff97144
sammy6378 <samuel202mwangi@gmail.com>
2025-03-18 16:27:54 +0300

    feat: add auth context provider for managing activation token state

---
+++ client/src/context/authContext.tsx
@@ -0,0 +1,30 @@
+
+
+import {createContext,Dispatch,SetStateAction,useContext,useState} from "react";
+
+
+interface IContext {
+  activationToken: string | null;
+  setActivationToken: Dispatch<SetStateAction<string | null>>;
+}
+
+
+export const AppContext = createContext<IContext | undefined>(undefined);
+
+export default function ProviderFunction({children}: {children: React.ReactNode}) {
+  const [activationToken, setActivationToken] = useState<string | null>(null);
+
+
+
+  return (
+    <AppContext.Provider value={{activationToken,setActivationToken}}>
+          {children}
+    </AppContext.Provider>
+  );
+}
+
+export const useContextFunc = () => {
+  const context = useContext(AppContext);
+  if (!context) throw new Error("Context not found");
+  return context;
+};
