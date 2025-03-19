032649c1ab3bf3d7e71a89c8a129d57c555e742b
sammy6378 <samuel202mwangi@gmail.com>
2025-03-19 23:58:18 +0300

    feat: add route for reporting issues with authentication middleware

--- server/Router/userRouter.ts
+++ server/Router/userRouter.ts
@@ -11,12 +11,14 @@ import {
   updateTriviaNumbers,
 } from "../Controlers/userControler";
 import requireAuth from "../middleware/authmiddleware";
+import { createIssue } from "../Controlers/issueController";
 const userRouter = Router();
 userRouter.route("/getUsers").get(getAll);
 userRouter.route("/createUser").post(createUser);
 userRouter.put("/update-avatar/:id",updateUserAvatar);
 userRouter.put("/update-scores",requireAuth, updateScore);
 userRouter.put("/update-trivias",requireAuth,updateTriviaNumbers);
+userRouter.post("/report-issue", requireAuth, createIssue);
 userRouter.get("/get-scores",getAllScores);
 userRouter.route("/:id").get(getOne).patch(updateUser).delete(deleteUser);
 
