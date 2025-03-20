d3a9f8acc75bd0e8555322ad7b05a70477a1b2ac
sammy6378 <samuel202mwangi@gmail.com>
2025-03-20 23:48:12 +0300

    chore: add Chart component for data visualization in admin dashboard

---
+++ client/src/Admin/components/Chart.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+const Chart = ({ data, title, grid, dataKey }: { data: any; title: string; grid: boolean; dataKey: string }) => {
+  return (
+    <div className="m-5 p-5 shadow-md">
+      <h3 className="mb-5 text-lg font-semibold">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
