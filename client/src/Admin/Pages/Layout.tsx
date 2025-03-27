ca7653acb05039162fe9e9f88508bdefdf883945
sammy6378 <samuel202mwangi@gmail.com>
2025-03-27 10:52:16 +0300

    feat: enhance AdminLayout with responsive sidebar and toggle functionality

--- client/src/Admin/Pages/Layout.tsx
+++ client/src/Admin/Pages/Layout.tsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Topbar from "../components/Topbar";
 import Sidebar from "../components/Sidebar";
 
-const adminLayout = ({ children }: { children: React.ReactNode }) => {
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <>
-      <Topbar />
-      <div className="flex">
-        <div className="w-64 h-screen">
-          <Sidebar />
-        </div>
-        <div className="flex-1 overflow-y-auto">
-          {children}
+    <div className="flex h-screen">
+      {/* Sidebar - Show only when open on small screens */}
+      <div
+        className={`fixed left-0 top-0 w-64 h-screen text-white transform transition-transform duration-300 ease-in-out 
+        ${isSidebarOpen ? "translate-x-0 z-[100]" : "-translate-x-full"} lg:translate-x-0`}
+      >
+        <Sidebar isOpen={isSidebarOpen} closeSidebar={() => setIsSidebarOpen(false)} />
+      </div>
+
+      {/* Main Content (Topbar + Page Content) */}
+      <div className={`flex-1 flex flex-col transition-all duration-300 ${isSidebarOpen ? "" : "lg:ml-64"}`}>
+        {/* Topbar */}
+        <Topbar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+
+        {/* Page Content */}
+        {/* <div className="flex-1 h-screen overflow-y-auto p-4 bg-gray-100">{children}</div> */}
+         {/* Page Content */}
+         <div className="flex-1 overflow-y-auto p-4 bg-gray-100">
+          <div className="max-w-full overflow-x-hidden">
+            {children}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default adminLayout;
+export default AdminLayout;
