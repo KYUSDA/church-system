f27466abb1d06ba1c89a76363b1f708d6bb7156d
sammy6378 <samuel202mwangi@gmail.com>
2025-03-28 11:22:40 +0300

    feat: add authentication and role-based access control to user routes

--- server/Router/userRouter.ts
+++ server/Router/userRouter.ts
@@ -10,15 +10,17 @@ import {
   getAllScores,
   updateTriviaNumbers,
 } from "../Controlers/userControler";
-import requireAuth from "../middleware/authmiddleware";
+import requireAuth, { authorizeRoles } from "../middleware/authmiddleware";
 import { createIssue, getIssue, getIssues, updateIssue } from "../Controlers/issueController";
 const userRouter = Router();
-userRouter.route("/getUsers").get(getAll);
+userRouter.route("/get-user/:id").get(getOne);
+userRouter.route("/getUsers").get(requireAuth,authorizeRoles("admin"),getAll);
 userRouter.patch("/update-user/:id",updateUser);
-userRouter.route("/createUser").post(createUser);
+userRouter.route("/createUser").post(requireAuth, authorizeRoles("admin"),createUser);
 userRouter.put("/update-avatar/:id",updateUserAvatar);
 userRouter.put("/update-scores",requireAuth, updateScore);
 userRouter.put("/update-trivias",requireAuth,updateTriviaNumbers);
+userRouter.delete("/delete-user/:id",requireAuth, authorizeRoles("admin"),deleteUser)
 
 // issues
 userRouter.post("/report-issue", requireAuth, createIssue);
@@ -28,6 +30,5 @@ userRouter.patch("/update-issue/:id",updateIssue);
 
 // scores
 userRouter.get("/get-scores",getAllScores);
-userRouter.route("/:id").get(getOne).delete(deleteUser);
 
 export default userRouter;
