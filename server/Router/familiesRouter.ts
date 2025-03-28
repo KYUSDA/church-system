69faefff149f94218d8597533ba9c45382676c33
sammy6378 <samuel202mwangi@gmail.com>
2025-03-28 11:22:16 +0300

    feat: add authentication and role-based access control to family routes

--- server/Router/familiesRouter.ts
+++ server/Router/familiesRouter.ts
@@ -6,13 +6,14 @@ import {
   updateFamily,
   deleteFamily,
 } from "../Controlers/family";
+import requireAuth,{ authorizeRoles } from "../middleware/authmiddleware";
 
 const familyRouter = Router();
 
-familyRouter.route("/getFamilies").get(getAll);
-familyRouter.route("/createFamily").post(createFamily);
-familyRouter.patch("/update-family/:id", updateFamily);
+familyRouter.route("/getFamilies").get(requireAuth, authorizeRoles("admin"), getAll);
+familyRouter.route("/createFamily").post(requireAuth, authorizeRoles("admin"),createFamily);
+familyRouter.patch("/update-family/:id", requireAuth, authorizeRoles("admin"), updateFamily);
 
-familyRouter.route("/:id").get(getOne).delete(deleteFamily);
+familyRouter.route("/:id").get(requireAuth, authorizeRoles("admin"),getOne).delete(authorizeRoles("admin"),deleteFamily);
 
 export default familyRouter;
