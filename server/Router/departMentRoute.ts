8dea399fc525a9741c020af7fdb521d6a1935ea0
sammy6378 <samuel202mwangi@gmail.com>
2025-03-28 11:22:07 +0300

    feat: implement authentication and role-based access control for department routes

--- server/Router/departMentRoute.ts
+++ server/Router/departMentRoute.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import departmentDetail from "../Controlers/departmentControler";
+import requireAuth,{ authorizeRoles } from "../middleware/authmiddleware";
 const departDetail = Router();
 departDetail
   .route("/:id")
-  .delete(departmentDetail.deleteDepertment)
+  .delete(requireAuth,authorizeRoles("admin"),departmentDetail.deleteDepertment)
 
-departDetail.route("/createDep").post(departmentDetail.createDep);
-departDetail.patch("/update-department/:id", departmentDetail.updateDepartment);
+departDetail.post("/createDep",requireAuth, authorizeRoles("admin"), departmentDetail.createDep);
+departDetail.patch("/update-department/:id",requireAuth, authorizeRoles("admin"), departmentDetail.updateDepartment);
 
 departDetail
   .route("/get-all-departments")
-  .get(departmentDetail.getAllDepartments)
-  .post(departmentDetail.getDepartmentDetails);
+  .get(requireAuth,authorizeRoles("admin"), departmentDetail.getAllDepartments)
+  .post(requireAuth,authorizeRoles("admin"), departmentDetail.getDepartmentDetails);
 
 export default departDetail;
