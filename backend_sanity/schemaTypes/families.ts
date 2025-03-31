3d34f9ab89a0c9d6753b60143dcb7c5fcfcb64a0
0tieno <ronvi035@gmail.com>
2025-03-31 22:16:08 +0300

    fixing sanity docker

--- backend_sanity/schemaTypes/families.ts
+++ backend_sanity/schemaTypes/families.ts
@@ -1,106 +1,118 @@
-export default {
-  name: "departments",
-  title: "Departments",
+import { defineField, defineType } from "sanity";
+
+export const families = defineType({
+  name: "families",
+  title: "Families",
   type: "document",
   fields: [
-    {
+    defineField({
       name: "title",
       title: "Title",
       type: "string",
-    },
-    {
-      name: "description",
-      title: "Description",
-      type: "string",
-    },
-    {
+    }),
+    defineField({
       name: "link",
       title: "Link",
       type: "string",
-    },
-    {
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
       name: "imgUrl",
       title: "ImageUrl",
       type: "image",
       options: {
         hotspot: true,
       },
-    },
-    {
+    }),
+    defineField({
+      name: "locationUrl",
+      title: "LocationUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
       name: "tags",
       title: "Tags",
       type: "array",
       of: [
-        {
+        defineField({
           name: "tag",
           title: "Tag",
           type: "string",
-        },
+        }),
       ],
-    },
-    {
+    }),
+    defineField({
       name: "leaders",
       title: "Leaders",
       type: "array",
       of: [
-        {
+        defineField({
           name: "leader",
           title: "Leader",
           type: "object",
           fields: [
-            {
+            defineField({
               name: "name",
               title: "Name",
               type: "string",
-            },
-            {
+            }),
+            defineField({
               name: "leaderUrl",
               title: "LeaderUrl",
               type: "image",
               options: {
                 hotspot: true,
               },
-            },
-            {
+            }),
+            defineField({
               name: "description",
               title: "Description",
               type: "string",
-            },
+            }),
           ],
-        },
+        }),
       ],
-    },
-    {
+    }),
+    defineField({
       name: "members",
       title: "Members",
       type: "array",
       of: [
-        {
+        defineField({
           name: "member",
           title: "Member",
           type: "object",
           fields: [
-            {
+            defineField({
               name: "name",
               title: "Name",
               type: "string",
-            },
-            {
+            }),
+            defineField({
               name: "memberUrl",
               title: "MemberUrl",
               type: "image",
               options: {
                 hotspot: true,
               },
-            },
-            {
+            }),
+            defineField({
               name: "description",
               title: "Description",
               type: "string",
-            },
+            }),
           ],
-        },
+        }),
       ],
-    },
+    }),
   ],
-};
+});
+
+export default families;
---
+++ backend_sanity/schemaTypes/index.ts
@@ -0,0 +1,20 @@
+import { about } from "./abouts";
+import { announcements } from "./announcements";
+import contact from "./contact";
+import departments from "./departments";
+import families from "./families";
+import skills from "./skills";
+import testimonials from "./testimonials";
+
+
+
+
+export const schemaTypes = [
+    about,
+    announcements,
+    contact,
+    departments,
+    families,
+    skills,
+    testimonials
+]
