1ea83415cf31b885110986652baa4ec4607ab4f5
sammy6378 <samuel202mwangi@gmail.com>
2025-06-10 21:25:18 +0300

    Add authSlice to manage authentication state and user data

---
+++ client/src/session/userSlice.ts
@@ -0,0 +1,68 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User{
+  id: string;
+  role: string;
+}
+
+interface AuthState {
+  accessToken: string | null;
+  isAuthenticated: boolean;
+  user: User | null;
+  expiresAt: number | null;
+}
+
+const initialState: AuthState = {
+  accessToken: null,
+  isAuthenticated: false,
+  user: null,
+  expiresAt: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (
+      state,
+      action: PayloadAction<{
+        user: User;
+        accessToken: string;
+        expiresIn: number;
+      }>
+    ) => {
+      state.user = action.payload.user;
+      state.accessToken = action.payload.accessToken;
+      state.isAuthenticated = true;
+      state.expiresAt = Date.now() + action.payload.expiresIn * 1000;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.accessToken = null;
+      state.isAuthenticated = false;
+      state.expiresAt = null;
+    },
+    updateTrivias: (
+      state,
+      action: PayloadAction<{ key: string; number: number }>
+    ) => {
+      if (state.user && action.payload.key in state.user) {
+        state.user = {
+          ...state.user,
+          [action.payload.key]: action.payload.number,
+        };
+      }
+    },
+    updateAuthToken: (
+      state,
+      action: PayloadAction<{ accessToken: string; expiresAt: number }>
+    ) => {
+      state.accessToken = action.payload.accessToken;
+      state.expiresAt = action.payload.expiresAt;
+    },
+  },
+});
+
+export const { login, logout, updateTrivias, updateAuthToken } =
+  authSlice.actions;
+export default authSlice.reducer;
