811759725eccef3f6b091c366b8f644edec9111b
sammy6378 <samuel202mwangi@gmail.com>
2025-08-03 21:57:34 +0300

    fix: remove trailing slash from allowed CORS origin URL

--- server/app.ts
+++ server/app.ts
@@ -31,7 +31,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://church-system-three.vercel.app/",
+  "https://church-system-three.vercel.app",
 ];
 
 app.use(
