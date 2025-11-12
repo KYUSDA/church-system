c84cfa3fe6d5a423f216094755d091e265dab654
Lameck Wafula <brianmontes723@gmail.com>
2025-11-12 11:44:09 +0300

    remove unused files

--- server/Router/xRoute.ts
+++ server/Router/xRoute.ts
@@ -3,6 +3,7 @@ import { getLatestXPosts } from '../Controlers/xController';
 
 const router = express.Router();
 
+// x route to get latest X posts
 router.get('/', getLatestXPosts);
 
 export default router;
