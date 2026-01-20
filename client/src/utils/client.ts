9401f7d083a3412100c48658e5fcf1d64dfbb106
sammy6378 <samuel202mwangi@gmail.com>
2026-01-20 14:50:42 +0300

    add api version for sanity

--- client/src/utils/client.ts
+++ client/src/utils/client.ts
@@ -4,12 +4,11 @@ import imageUrlBuilder from '@sanity/image-url';
 const projectId = import.meta.env.VITE_SANITY_PROJECT_ID;
 const dataset = import.meta.env.VITE_SANITY_DATASET;
 const token = import.meta.env.VITE_SANITY_API_TOKEN;
-const version = import.meta.env.VITE_SANITY_API_VERSION;
 
 export const client = sanityClient({
     projectId: projectId,
     dataset: dataset,
-    apiVersion: version,
+    apiVersion: '2022-02-01',
     useCdn: true,
     token: token,
 });
